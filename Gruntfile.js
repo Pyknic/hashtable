fcc86c7c8e94d37e70e5292eaf019e9cc9603f7a
pyknic <emil@speedment.com>
2017-02-26 10:06:50 -0800

    Add Closure options to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,20 @@ module.exports = function (grunt) {
     // Project configuration.
     grunt.initConfig({
         'closure-compiler': {
-            security: {
+            hastable: {
                 closurePath  : '/home/pyknic/closure_compiler',
                 js           : 'src/hashtable.js',
                 jsOutputFile : 'dist/hashtable.min.js',
                 maxBuffer    : 500,
+                files : {
+                    'dist/hashtable.min.js': ['src/**/*.js']
+                },
                 options      : {
+                    manage_closure_dependencies : true,
                     compilation_level : 'ADVANCED_OPTIMIZATIONS',
-                    language_in       : 'ECMASCRIPT6_STRICT'
+                    language_in       : 'ECMASCRIPT6_STRICT',
+                    create_source_map : 'dist/hashtable.min.js.map',
+                    output_wrapper    : '(function(){\n%output%\n}).call(this)\n//# sourceMappingURL=hashtable.min.js.map'
                 }
             }
         }
